6baf743aa0ca48273dd8f2301f12f28e0b70b8cc
victor.delarosa <1938685+vdlr@users.noreply.github.com>
2025-08-28 15:39:47 +0200

    markdown support added

--- src/xygeni/common/markdown.ts
+++ src/xygeni/common/markdown.ts
@@ -1,13 +1,10 @@
-
-
-
-
 export class MarkdownParser {
 
   static marked_instance: any;
 
   static parse(text: string): string {
     if (!this.marked_instance) {
+      // marked library doesn't support commonJS module types
       const getMarked = require('marked');
       this.marked_instance = getMarked;
     }
